5b48c3f59b2f8f1a5a120fe5860d788a0debf87f
Maximilien Kpizingui <kpiz1@hotmail.com>
2024-03-04 13:50:13 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,37 @@
 import streamlit as st
 import torch
 import pandas as pd
+import torch.nn as nn
 
+# Define your model class
+class SpatialConvNet(nn.Module):
+    def __init__(self, input_channels, output_channels):
+        super(SpatialConvNet, self).__init__()
+        self.conv1 = nn.Conv2d(input_channels, 64, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        self.conv3 = nn.Conv2d(128, output_channels, kernel_size=3, padding=1)
+        self.relu = nn.ReLU()
+        self.maxpool = nn.MaxPool2d(kernel_size=2, stride=2)
 
-model = torch.load('trained_model.pth')
+    def forward(self, x):
+        x = self.relu(self.conv1(x))
+        x = self.maxpool(x)
+        x = self.relu(self.conv2(x))
+        x = self.maxpool(x)
+        x = self.relu(self.conv3(x))
+        return x
+
+# Load the model state dictionary
+model_state_dict = torch.load('trained_model.pth')
+
+# Instantiate your model class
+input_channels = 75  # Update with the appropriate number of input channels
+output_channels = 1   # Update with the appropriate number of output channels
+model = SpatialConvNet(input_channels=input_channels, output_channels=output_channels)
+
+# Load the model's state dictionary
+model.load_state_dict(model_state_dict)
+model.eval()  # Set the model to evaluation mode
 
 station_columns = ['station_baguera', 'station_banfora_agri', 'station_baraboule', 'station_batie', 'station_bereba', 'station_beregadougou', 'station_betare', 'station_bogande', 'station_bousse', 'station_dedougou', 'station_diebougou', 'station_dionkele', 'station_fada_ngourma', 'station_gayeri', 'station_hounde', 'station_kamboince', 'station_koupela', 'station_leo', 'station_leri', 'station_mane', 'station_mogtedo', 'station_nobere', 'station_pabre', 'station_piela', 'station_po', 'station_pobe_mengao', 'station_reo_agri', 'station_safane', 'station_sapone', 'station_seguenega', 'station_sideradougou', 'station_soubakaniedougou', 'station_tanghin_dassouri', 'station_tikare', 'station_toeni', 'station_vallee_du_kou', 'station_zabre']
 
